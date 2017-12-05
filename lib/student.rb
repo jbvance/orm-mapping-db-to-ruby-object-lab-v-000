e9ce1346780d1a5c1f86de5f7121b7ac053ef974
Jason Vance <jbvance@gmail.com>
2017-12-05 14:18:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    self.first_X_students_in_grade_10(1)
+    self.first_X_students_in_grade_10(1).flatten
   end
 
   def self.find_by_name(name)
